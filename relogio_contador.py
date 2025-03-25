b965641eb1a9feb660e8050fccd3a919d2927341
Davebenicio1 <messiunifieo@gmail.com>
2025-03-25 17:27:24 -0300

    Adicionando o relógio contador

---
+++ relogio_contador.py
@@ -0,0 +1,14 @@
+import time
+
+def relogio_contador(segundos):
+    while segundos:
+        minutos, segs = divmod(segundos, 60)
+        timer = f'{minutos:02}:{segs:02}'
+        print(timer, end='\r')
+        time.sleep(1)
+        segundos -= 1
+    print('Tempo esgotado!')
+
+if __name__ == "__main__":
+    tempo_inicial = int(input("Digite o tempo em segundos: "))
+    relogio_contador(tempo_inicial)
